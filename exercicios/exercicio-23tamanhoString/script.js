f105ec65ea185953c19956da3d11b34a134ce4b4
ingrid <ingridmarianb92@gmail.com>
2024-01-17 09:25:21 -0300

    obter tamanho da string

---
+++ exercicios/exercicio-23tamanhoString/script.js
@@ -0,0 +1,11 @@
+function tamanhoTexto (texto){
+    if(texto.length > 10){
+        console.log("texto muito grande")
+    }else{
+        console.log("texto dentro do limite")
+    }
+}
+
+tamanhoTexto("texto");
+tamanhoTexto("testando para ver se funciona");
+tamanhoTexto("oi");
